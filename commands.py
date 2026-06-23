32d0fe7b0762ca4b2a83b653a46e501420a28e31
Ravikantjha750 <ravikantjha750@gmail.com>
2026-06-23 20:22:56 +0530

    Added Desktop Application Launcher

--- commands.py
+++ commands.py
@@ -1,14 +1,46 @@
 import webbrowser
+import subprocess
 
 def execute_command(command):
 
-    if command.startswith("open "):
 
-        site = command.replace("open ", "").strip().lower()
 
-        url = f"https://www.{site}.com"
+    command = command.lower()
 
-        webbrowser.open(url)
+    if command == "open notepad":
+        subprocess.Popen("notepad")
+        return "Opening Notepad"
+
+    elif command == "open calculator":
+        subprocess.Popen("calc")
+        return "Opening Calculator"
+
+
+    elif (
+
+            command == "open vscode"
+
+            or command == "open vs code"
+
+            or command == "open visual studio code"
+            or command == "open v s code"
+            or command == "open v scode"
+            or command == "open vs"
+
+    ):
+
+        subprocess.Popen(
+
+            r"C:\Users\Ravikant Jha\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+
+        )
+
+        return "Opening VS Code"
+
+    elif command.startswith("open "):
+        site = command.replace("open ", "").strip()
+
+        webbrowser.open(f"https://www.{site}.com")
 
         return f"Opening {site}"
 
---
+++ test_vscode.py
@@ -0,0 +1,5 @@
+import subprocess
+
+subprocess.Popen(
+    r"C:\Users\Ravikant Jha\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+)
